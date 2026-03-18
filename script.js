b415ea2348ddbb7aa5a8565702a3e84a265edb30
aimi-takanaka <1maime.to@gmail.com>
2026-03-18 23:05:40 +0900

    pokemon map
    ポケモンを配列とマップで並べるように変更途中

--- script.js
+++ script.js
@@ -3,8 +3,22 @@
 const section = document.getElementById('pokemon');
 
 for (let i = 1; i <= 9; i++) {
-  const pokemon = document.createElement('div');//<div> タグを新しく1個作る。まだHTMLには追加していない、メモリの中だけにある状態。
-  pokemon.classList.add('pokemon');//CSSを当てるための準備
+
+  const topRow = [1, 2, 3, 4, 5, 6];
+  const bottomRow = [7, 8, 9];
+
+  function creatRow(number) {
+    const row = document.createElement('div');
+    row.classList.add('pokemon-row');
+
+    number.map(i => {
+      const pokemon = document.createElement('div');
+      pokemon.classList.add('pokemon');
+
+      // 途中です！！！
+
+    })
+  }
 
   const newImg = document.createElement('img');
   newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
