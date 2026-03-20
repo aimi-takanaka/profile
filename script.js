7edbfd45b5fe22174e85b70e410ae44733a2ec0a
aimi-takanaka <1maime.to@gmail.com>
2026-03-20 12:24:07 +0900

    pokemon
    pokemon　完成！

--- script.js
+++ script.js
@@ -2,29 +2,27 @@
 
 const section = document.getElementById('pokemon');
 
-for (let i = 1; i <= 9; i++) {
+const topRow = [1, 2, 3, 4, 5, 6];
+const bottomRow = [7, 8, 9];
 
-  const topRow = [1, 2, 3, 4, 5, 6];
-  const bottomRow = [7, 8, 9];
+function createRow(numbers) {
+  const row = document.createElement('div');
+  row.classList.add('pokemon-row');
 
-  function creatRow(number) {
-    const row = document.createElement('div');
-    row.classList.add('pokemon-row');
+  numbers.map(i => {
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon');
 
-    number.map(i => {
-      const pokemon = document.createElement('div');
-      pokemon.classList.add('pokemon');
+    const newImg = document.createElement('img');
+    newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
 
-      const newImg = document.createElement('img');
-      newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
-      // 途中です！！！
+    pokemon.appendChild(newImg);
+    row.appendChild(pokemon);
+  });
 
-    })
-  }
+  return row;
 
-
-
-
-  pokemon.appendChild(newImg);
-  section.appendChild(pokemon);
 }
+
+section.appendChild(createRow(topRow));
+section.appendChild(createRow(bottomRow));
